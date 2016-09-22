601dbde34fc61aa04d9631102dc35751be158b35
Alexbo05 <aluno@academico.ba.senac.br>
2016-09-22 18:02:14 -0300

    New LevelDesign

--- Bacon´s Nightmare/Assets/Scripts/CobraController.cs
+++ Bacon´s Nightmare/Assets/Scripts/CobraController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class CobraController : MonoBehaviour 
 {
+	/*
 	Animator anim;
 	bool walkLimit;
 	Vector2 posInicial;
@@ -45,3 +46,51 @@ public class CobraController : MonoBehaviour
 		transform.Translate (new Vector2 (3, 0) * Time.deltaTime);
 	}
 }
+*/
+		private Vector3 posA;
+
+		private Vector3 posB;
+
+		private Vector3 nexPos;
+
+		[SerializeField]
+		private float speed;
+
+		[SerializeField]
+		private Transform childTransform;
+
+		[SerializeField]
+		private Transform transformB;
+
+		[SerializeField]
+		private Transform transformA;
+
+		// Use this for initialization
+		void Start () {
+
+			//posA = childTransform.localPosition;
+			posA = transformA.localPosition;
+			posB = transformB.localPosition;
+
+			nexPos = posB;
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+			Move ();
+		}
+
+		private void Move ()
+		{
+			childTransform.localPosition = Vector3.MoveTowards (childTransform.localPosition, nexPos, speed * Time.deltaTime);
+			if (Vector3.Distance (childTransform.localPosition, nexPos) <= 0.1) {
+				ChangeDestination ();
+			}
+
+		}
+		private void ChangeDestination(){
+			//		nexPos = nexPos != posA ? posA : posB;
+			nexPos = nexPos != posA ? posA : posB;
+		}
+	}
