d7da73c3ee77d655d4486859da574c3b15b8a79f
Gabrielbcrc <gabrielbcrc@gmail.com>
2016-09-23 17:34:41 -0300

    CUTSCENE
    Cutscene criada

---
+++ Bacon´s Nightmare/Assets/cutscene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class cutscene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	public void vaiPlaneta()
+	{
+		SceneManager.LoadScene("Level 02 - alpha");
+	}
+}
