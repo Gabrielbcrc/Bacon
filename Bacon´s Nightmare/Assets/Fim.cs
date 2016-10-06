e6d9b7bae163d696016d71b3eb9d8f2ff15b72ff
Thiago-Morais <thiolimor@gmail.com>
2016-10-06 02:33:28 -0300

    Fin

---
+++ Bacon´s Nightmare/Assets/Fim.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Fim : MonoBehaviour {
+
+	public Antonito porco;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if(other.tag == "Player")
+		{
+			porco.blockPlayer ();
+			PlayerPrefs.SetInt ("passou",1);
+
+			Invoke ("load", 1);
+		}
+	}
+	void load()
+	{
+		Debug.Log (porco.cenaNumero);
+		SceneManager.LoadScene ("Fim");
+	}
+}
