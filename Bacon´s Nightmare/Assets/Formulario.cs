d0efc2e2019571fa4ba008f15304ffe4f50208c0
Thiago-Morais <thiolimor@gmail.com>
2016-10-06 03:32:42 -0300

    RGB bug 3:32

---
+++ Bacon´s Nightmare/Assets/Formulario.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Formulario : MonoBehaviour 
+{
+
+	void OnMouseDown(){
+//		GameObject.Find("Fade").GetComponent<Animator>().Play("FadeOut");
+//		Invoke("LoadScreen", 1);
+		Application.OpenURL ("https://docs.google.com/forms/d/e/1FAIpQLSfThwtSix09sYqF5XLSq9uOoUs903Qx30Gc763GFmFKqbPSKQ/viewform");
+	}
+
+	void OnMouseExit(){
+		this.GetComponent<UnityEngine.UI.Text>().color = new Color (10,10,10)/*(16,35,165)*/;
+	}
+
+	void OnMouseOver(){
+		this.GetComponent<UnityEngine.UI.Text>().color = new Color (10,0,0);
+	}
+
+	void LoadScreen(){
+		SceneManager.LoadScene("Night Level Design");
+	}
+}
