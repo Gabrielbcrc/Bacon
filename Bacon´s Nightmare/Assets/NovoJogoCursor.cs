b55dddf594116cd62335d10eef5353d522541649
Gabriel Britto <gabrielbcrc@gmail.com>
2016-10-06 01:49:02 -0300

    Modificações Menu 1:49

---
+++ Bacon´s Nightmare/Assets/NovoJogoCursor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NovoJogoCursor : MonoBehaviour 
+{
+
+	void OnMouseDown() 
+	{
+		GameObject.Find("Fade").GetComponent<Animator>().Play("FadeOut");
+		Invoke("LoadScreen", 1);
+	}
+
+	void OnMouseOver()
+	{
+		this.GetComponent<SpriteRenderer>().color = new Color (255,0,0);
+	}
+
+	void LoadScreen()
+	{
+		SceneManager.LoadScene("Night Level Design");
+	}
+}
---
+++ Bacon´s Nightmare/Assets/SairCursor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SairCursor : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Application.Quit();
+	}
+
+	void OnMouseOver()
+	{
+		this.GetComponent<SpriteRenderer>().color = new Color (255,0,0);
+	}
+}
